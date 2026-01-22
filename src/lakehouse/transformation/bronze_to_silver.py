77f73bdfaaac0594caa9eab747d0ef7031773c7a
mohamedmgh <mohamedmghezzi2@gmail.com>
2026-01-22 13:55:24 +0100

    corrige erreur de path et de donnes db

--- src/lakehouse/transformation/bronze_to_silver.py
+++ src/lakehouse/transformation/bronze_to_silver.py
@@ -2,15 +2,20 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import sum as spark_sum
 from pyspark.sql.functions import col
 
+
 spark = (
     SparkSession.builder
     .appName("S3 Parquet Analysis")
     .getOrCreate()
 )
 
-spark
 
-s3_path = "s3a://your bucket s3/bronze/Contrat2/"
+resolver = PathResolver()
+
+s3_path = resolver.s3_layer_path(
+    layer="bronze",
+    dataset="Contrat2"
+)
 
 df = spark.read.parquet(s3_path)
 
@@ -21,14 +26,7 @@ df.select("nusoc", "nucon", "cateco", "prmaco").show(5)
 df.groupBy("cateco").count().show() #A auto , C=Cycle , M=moto
 df.groupBy("markli").agg({"prmaco": "avg"}).orderBy("avg(prmaco)", ascending=False).show(10)
 
-from pyspark.sql.functions import col
 
-garanties = [
-    "g01co","g02co","g03co","g04co","g05co","g06co",
-    "g09co","g10co","g13co","g15co","g16co","g17co",
-    "g18co","g19co","g21co","g22co","g23co","g25co",
-    "g26co","g28co"
-]
 
 for g in garanties:
     df = df.withColumn(g, col(g).cast("int"))
